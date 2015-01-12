1876a6671a8c1435fd6affb69272d62e9d5a5b1a
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-12 18:50:09 +0000

    Added interface for User class

---
+++ src/exercise1_2/User.java
@@ -0,0 +1,21 @@
+package exercise1_2;
+
+public interface User {
+	/**
+	 * Returns user's name
+	 * @return the user's name
+	 */
+	public String getName();
+	
+	/**
+	 * Return's the user's id
+	 * @return the user's id
+	 */
+	public int getId();
+	
+	/**
+	 * Set's the user's id 
+	 * @param id the user's id
+	 */
+	public void setId(int id);
+}
