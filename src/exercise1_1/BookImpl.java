1e8829393d7dccd76df9d9f2e00263494cbf1d75
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-12 18:44:35 +0000

    Implemented book as BookImpl

---
+++ src/exercise1_1/BookImpl.java
@@ -0,0 +1,22 @@
+package exercise1_1;
+
+public class BookImpl implements Book {
+	private String author;
+	private String title;
+	
+	public BookImpl(String title, String author){
+		this.title = title;
+		this.author = author;
+	}
+	
+	@Override
+	public String getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+}
