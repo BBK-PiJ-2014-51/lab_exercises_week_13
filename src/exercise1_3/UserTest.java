c1af1c3e05ae207f33a30a7fa55e43e18643d438
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-12 19:11:03 +0000

    added User tests

---
+++ src/exercise1_3/UserTest.java
@@ -0,0 +1,29 @@
+package exercise1_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exercise1_1.Book;
+import exercise1_1.BookImpl;
+
+public class UserTest {
+private User user;
+	
+	@Before
+	public void buildUser(){
+		user = new UserImpl("Joe");
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Joe", user.getName());
+	}
+	
+	@Test
+	public void testGetAndSetId() {
+		user.setId(99);
+		assertEquals(99, user.getId());
+	}
+}
