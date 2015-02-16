e7c59fed59a68d39899ddaa3ae2cf031cbb60aca
caleb <clayton.caleb@gmail.com>
2015-02-16 21:00:54 +0000

    added failing tests for exercise 1.6

--- src/exercise1_6/LibraryTest.java
+++ src/exercise1_6/LibraryTest.java
@@ -9,11 +9,14 @@ import org.junit.Test;
 public class LibraryTest {
 	Library library;
 	User user;
+	Book book;
+	
 	
 	@Before
 	public void build(){
 		library = new LibraryImpl("Library A");
 		user = new UserImpl("Joe");
+		book = new BookImpl("The Old Man and The Sea", "Ernest Hemmingway");
 	}
 	
 	@Test
@@ -32,5 +35,29 @@ public class LibraryTest {
 		user.register(library);
 		int id = user.getId();
 		assertEquals(id, library.getId(user.getName()));
+	}
+	
+	@Test
+	public void takeBook(){
+		library.addBook(book.getTitle(), book.getAuthor());
+		Book returnedBook = library.takeBook(book.getTitle());
+		assertEquals(book.getAuthor(), returnedBook.getAuthor());
+	}
+	
+	@Test
+	public void takeBookBeforeReturn(){
+		library.addBook(book.getTitle(), book.getAuthor());
+		Book returnedBook = library.takeBook(book.getTitle());
+		returnedBook = library.takeBook(book.getTitle());
+		assertEquals(null, returnedBook);
+	}
+	
+	@Test
+	public void returnBook(){
+		library.addBook(book.getTitle(), book.getAuthor());
+		Book returnedBook = library.takeBook(book.getTitle());
+		library.returnBook(returnedBook);
+		returnedBook = library.takeBook(book.getTitle());
+		assertEquals(book.getAuthor(), returnedBook.getAuthor());
 	}
 }
